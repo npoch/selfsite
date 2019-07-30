73b76a7c70a2f298759502599bdf7b9f38a0b3dd
Nathan Poch <npoch@hubba.com>
2019-07-29 22:12:17 -0400

    added jquery shim

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-const header = html('_header.html');
-const footer = html('_footer.html');
+requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'jquery-1.9.0'
+    }
+});
 
-(function(){
-    $('body').prepend($('header'));
-    $('body').append('footer');
-})();
+const htmlHeader = require('../Includes/_header.html');
+
+$(function(){
+    console.log('why isn\'t this working');
+    // $('header').load('_header.html');
+    $('body').prependTo(htmlHeader);
+  });
